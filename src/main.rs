a6551fad3c330c378a5373f3d506dd96ab92b95c
Yuri-M-Dias <yurimathe.yp@gmail.com>
2020-12-22 23:54:30 -0300

    Forgot rust fmt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-  let listener = TcpListener::bind("127.0.0.1:0")
-    .expect("Failed to bind random port");
-  let port = listener.local_addr().unwrap().port();
-  println!("Running on http://127.0.0.1:{}", port);
-  run(listener)?.await
+async :lovefn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Running on http://127.0.0.1:{}", port);
+    run(listener)?.await
 }
