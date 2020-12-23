f5bd58c69345b91a999df60d803355a60908bae3
Yuri-M-Dias <yurimathe.yp@gmail.com>
2020-12-23 00:03:29 -0300

    Oops emoji in the middle

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::net::TcpListener;
 use zero2prod::run;
 
 #[actix_rt::main]
-async :lovefn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     println!("Running on http://127.0.0.1:{}", port);
